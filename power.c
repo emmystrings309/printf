2d2781876a38d0b8cd789e31f1c618fcfe220459
emmystringzhs <milesbetti@gmail.com>
2023-10-16 19:38:18 +0100

    re edit the comment and also the var declaration

--- power.c
+++ power.c
@@ -3,10 +3,11 @@
 #include <stdarg.h>
 
 
-// Function to calculate power
+/* Function to calculate power */
 int power(int base, int exponent) {
-    int result = 1;
-    for (int i = 0; i < exponent; i++) {
+    int i, result;
+    result = 1;
+    for (i = 0; i < exponent; i++) {
         result *= base;
     }
     return result;
