fdc523c29e8698581eb9b940c3a713b8d4170964
emmystringzhs <milesbetti@gmail.com>
2023-10-16 19:37:38 +0100

    correct the code to run with gcc -Wall -Wextra -Werror -pedantic -std=gnu89 -Wno-format

--- _printf.c
+++ _printf.c
@@ -2,75 +2,80 @@
 #include "main.h"
 #include <stdarg.h>
 
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
+    int ch_printed, num, num_digits, temp, digit, i, j;
     va_list args;
     va_start(args, format);
-    int ch_printed = 0;
-    int num, num_digits, temp, digit;
-
-    for (int i = 0; format[i] != '\0'; i++) {
-        if (format[i] != '%') {
+    ch_printed = 0;
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
             _putchar(format[i]);
             ch_printed++;
-        } else {
-            // Handle format specifier
-            i++; // Move to the next character after '%'
+        }
+        else
+        {
+            /* Handle format specifier */
+            i++; /* Move to the next character after '%' */
 
-            if (format[i] == 'd' || format[i] == 'i') {
-                // Retrieve integer argument
+            if (format[i] == 'd' || format[i] == 'i')
+            {
+                /* Retrieve integer argument */
                 num = va_arg(args, int);
-                if (num < 0/* condition */)
+                if (num < 0 /* condition */)
                 {
                     /* code */
                     _putchar('-');
                     num = ABS(num);
                 }
-                
-                // Print each digit individually
+
+                /* Print each digit individually */
                 temp = num;
                 num_digits = 0;
 
-                // Count the number of digits in the number
-                while (temp != 0) {
+                /* Count the number of digits in the number */
+                while (temp != 0)
+                {
                     temp /= 10;
                     num_digits++;
                 }
 
-                // Extract each digit and print it
-                for (int j = num_digits - 1; j >= 0; j--) {
+                /* Extract each digit and print it */
+                for (j = num_digits - 1; j >= 0; j--)
+                {
                     digit = (num / power(10, j)) % 10;
-                    _putchar('0' + digit);  // Convert the digit to character and print
+                    _putchar('0' + digit); /* Convert the digit to character and print */
                     ch_printed++;
                 }
             }
-           if (format[i] == 's'/* condition */)
-           {
-            /* code */
-            const char *str = va_arg(args, const char *);
-
-            while (*str != '\0')
+            if (format[i] == 's' /* condition */)
             {
                 /* code */
-                _putchar(*str);
-                str++;
+                const char *str = va_arg(args, const char *);
+
+                while (*str != '\0')
+                {
+                    /* code */
+                    _putchar(*str);
+                    str++;
+                    ch_printed++;
+                }
+            }
+
+            if (format[i] == 'c' /* condition */)
+            {
+                /* code */
+                char cha = va_arg(args, int);
+                _putchar((char)cha);
                 ch_printed++;
             }
-            
-           }
-           
-           if (format[i] == 'c'/* condition */)
-           {
-            /* code */
-            char cha = va_arg(args, int);
-            _putchar((char)cha);
-            ch_printed++;
-           }
-            
-            // Add other format specifiers (e.g., %s, %c) as needed
+
+            /* Add other format specifiers (e.g., %s, %c) as needed */
         }
     }
 
     va_end(args);
     return ch_printed;
 }
-
