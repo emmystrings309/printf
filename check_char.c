1b492d5ad6051aef09818d3aaf48d105ed081537
emmystringzhs <milesbetti@gmail.com>
2023-10-17 09:47:03 +0100

    updated the comment to be more explicit

--- check_char.c
+++ check_char.c
@@ -2,9 +2,18 @@
 #include "main.h"
 #include <stdarg.h>
 
+/**
+ * check_char - print char value
+ * @cha: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
 int check_char(va_list args, int ch_printed)
 {
-    char cha = va_arg(args, int);
+    char cha;
+    cha = va_arg(args, int);
     _putchar((char)cha);
     ch_printed++;
     return ch_printed;
--- check_str.c
+++ check_str.c
@@ -2,9 +2,18 @@
 #include "main.h"
 #include <stdarg.h>
 
+/**
+ * check_str - writes the character c to stdout
+ * @str: The character pointer to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
 int check_str(va_list args, int ch_printed)
 {
-const char *str = va_arg(args, const char *);
+    const char *str;
+str = va_arg(args, const char *);
     while (*str != '\0')
     {
         /* code */
