9c1f506bfdcc8d3f5e499bec5d7527114cad3010
Alexa <onabanjotomiwa@gmail.com>
2023-10-17 14:12:20 +0100

    function for handling other format specifer

--- format_specifier.c
+++ format_specifier.c
@@ -4,5 +4,34 @@
 
 int format_specifier(int ch_printed, va_list args, const char *format, int i)
 {
-return 0;
+    switch (format[i]) {
+        case 'b': // Custom 'b' specifier for binary
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                int binary[32]; // Assuming 32-bit integers
+                int i = 0;
+
+                // Calculate binary representation
+                do {
+                    binary[i] = num % 2;
+                    num /= 2;
+                    i++;
+                } while (num > 0);
+
+                // Print binary digits in reverse order
+                for (i--; i >= 0; i--) {
+                    _putchar(binary[i] + '0');
+                    ch_printed++;
+                }
+            }
+            break;
+        default:
+            // Handle unsupported or unknown specifiers
+            _putchar('%'); // Print '%' character
+            _putchar(format[i]); // Print the unknown specifier
+            ch_printed += 2; // Increment the count by 2
+            break;
+    }
+
+    return ch_printed;
 }
