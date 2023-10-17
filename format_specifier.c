4914adf043c5c343a05cbc92354ac33354cd33a5
Alexa <onabanjotomiwa@gmail.com>
2023-10-17 12:36:41 +0100

    function for handling other format  specifer

--- format_specifier.c
+++ format_specifier.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <stdarg.h>
 
-int format_specifier(int ch_printed, va_list args, format[i])
+int format_specifier(int ch_printed, va_list args, const char *format[], int i)
 {
 return 0;
 }
