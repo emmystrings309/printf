be8ffff17bd35342a300c07d336ccb2a66ea1049
emmystringzhs <milesbetti@gmail.com>
2023-10-17 09:12:16 +0100

    updated the header files with the new prototype

--- main.h
+++ main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 #define ABS(x) ((x) < (0) ? -(x) : (x))
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 int power(int base, int exponent);
 int _putchar(char c);
+int check_int(va_list args, int ch_printed);
+int check_str(va_list args, int ch_printed);
+int check_char(va_list args, int ch_printed);
 
 #endif /* MAIN_H */
