03f7a0c71a96917fd061a784a55ee89f0527552b
emmystringzhs <milesbetti@gmail.com>
2023-10-17 09:10:35 +0100

    modularized the code to declare some logic else where

--- _printf.c
+++ _printf.c
@@ -19,63 +19,22 @@ else
 {
 /* Handle format specifier */
 i++; /* Move to the next character after '%' */
-
 if (format[i] == 'd' || format[i] == 'i')
 {
-/* Retrieve integer argument */
-num = va_arg(args, int);
-if (num < 0 /* condition */)
-{
-/* code */
-_putchar('-');
-num = ABS(num);
-}
-
-/* Print each digit individually */
-temp = num;
-num_digits = 0;
-
-/* Count the number of digits in the number */
-while (temp != 0)
-{
-temp /= 10;
-num_digits++;
-}
-
-/* Extract each digit and print it */
-for (j = num_digits - 1; j >= 0; j--)
-{
-digit = (num / power(10, j)) % 10;
-_putchar('0' + digit); /* Convert the digit to character and print */
-ch_printed++;
-}
+ch_printed = check_int(args, ch_printed);
 }
 if (format[i] == 's' /* condition */)
 {
 /*code*/
-const char *str = va_arg(args, const char *);
-
-while (*str != '\0')
-{
-/* code */
-_putchar(*str);
-str++;
-ch_printed++;
+ch_printed = check_str(args, ch_printed);
 }
-}
-
 if (format[i] == 'c' /* condition */)
 {
 /* code */
-char cha = va_arg(args, int);
-_putchar((char)cha);
-ch_printed++;
-}
-
-/* Add other format specifiers (e.g., %s, %c) as needed*/
+ch_printed = check_char(args, ch_printed);
 }
 }
-
+}
 va_end(args);
-return ch_printed ;
+return (ch_printed);
 }
